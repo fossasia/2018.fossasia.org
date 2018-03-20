6e2206c09591567928bd8d7e864aec5f8359bb9f
Prem Kumar <prmsrswt@protonmail.com>
2018-03-20 11:20:20 +0530

    Added JS for back to top button

--- js/scripts.js
+++ js/scripts.js
@@ -363,3 +363,27 @@ $(window).load(function() {
             return false;
         }   
 });
+
+/*********Back to Top Scripts*********/
+if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
+
